e0f901a0c2529302918db5aa5449278ed1981e3d
Jason Tan <jtan5662@bths.edu>
2018-10-02 14:35:40 -0400

    asdfgh

--- src/JtanLib.java
+++ src/JtanLib.java
@@ -1,2 +1,11 @@
 public class JtanLib {
+    public static boolean isPalindrome(String str1)
+    {
+        String reverse= "";
+        for (int i = str1.length() -1; i == 0; i--) {
+            reverse = reverse + str1.substring(str1.length() - i - 1, str1.length() - 1);
+        }
+        if (reverse.equals(str1)) return true;
+        else return false;
+    }
 }
