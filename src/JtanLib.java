fef8f89eb9bcff759d085f0b711a316a549f2a7e
Jason Tan <jtan5662@bths.edu>
2018-10-05 14:50:24 -0400

    im sick. hope it goes away

--- src/JtanLib.java
+++ src/JtanLib.java
@@ -1,6 +1,11 @@
 import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
-
+// Jason Tan
 public class JtanLib {
+    /**
+     * This checks if a word is a palindrome. A palindrome is a word that is spelt the same, even when you spell it backwards. For example, racecar is spelt racecar when you spell it backwards.
+     * @param str1 This is the word which the program checks to see if it is a palindrome.
+     * @return A boolean value of true if the word checked is a palindrome or a boolean value of false if the word isn't a palindrome.
+     */
     public static boolean isPalindrome(String str1) {
         String reverse = "";
         for (int i = str1.length(); i != 0; i--) {
@@ -12,6 +17,11 @@ public class JtanLib {
 
     }
 
+    /**
+     * This program gives the date in the order of day first, month second, and year last.
+     * @param Date This is the date entered in the order of month first, day second, and year last.
+     * @return The inserted date but with the position of date and month swapped so that day comes fist, then month second, and lastly year.
+     */
 
     public static String dateSTR(String Date) {
         String day = Date.substring(3,5);
@@ -21,7 +31,12 @@ public class JtanLib {
         return date;
     }
 
-
+    /**
+     *
+     * @param mainSTR
+     * @param subSTR
+     * @return
+     */
     public static String cutOut(String mainSTR, String subSTR) {
         int Cut = mainSTR.indexOf(subSTR);
         String part1 = mainSTR.substring(0, Cut);
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,9 +1,13 @@
+// Jason Tan
 public class TestFile {
     public static void main(String [] args) {
         System.out.println(JtanLib.isPalindrome("eevee"));
+        System.out.println(JtanLib.isPalindrome("eeve"));
         System.out.println(JtanLib.dateSTR("07/06/2001"));
         System.out.println(JtanLib.cutOut("pizzabizza boy", "pizza"));
-        System.out.println(JtanLib.isFibonacci(6));
+        System.out.println(JtanLib.isFibonacci(8));
+        System.out.println("s");
+        System.out.println(JtanLib.isFibonacci(20));
         System.out.println(JtanLib.sumUpTo(3));
     }
 
